755165439aac3dbfc0e7945ed7ef91108ae3132f
Julian <julian-jordan.starks@fwcsstudents.org>
2025-02-05 12:00:26 -0500

    Add files via upload

---
+++ Starks_TIY_10_1.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+path = Path('learning_python.txt')
+contents = path.read_text()
+
+print(contents)
+
+lines = contents.splitlines()
+py_string = ''
+for line in lines:
+    py_string += line.rstrip()
+
+print(f"py_string")
